498d9be1f37e37fafde702137885a094cefd3581
ekanshd <ekansh.dugar@gmail.com>
2023-02-14 12:56:29 +0530

    saved

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+}
+
+resource "aws_instance" "terraform-ec2" {
+  ami           = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+}
