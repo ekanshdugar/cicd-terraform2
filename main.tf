3d2de8b99a2ccca1d02fe42865ef3c9ff6452320
ekanshd <ekansh.dugar@gmail.com>
2023-02-14 16:54:32 +0530

    modified6

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-    access_key = "${env.AWS_ACCESS_KEY_ID}"
-    secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
+    access_key = "${terraformcicd.env.AWS_ACCESS_KEY_ID}"
+    secret_key = "${terraformcicd.env.AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_instance" "terraform-ec2" {
