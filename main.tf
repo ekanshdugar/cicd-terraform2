6abe22bd47013dabb1a161eda133b57412bfb631
ekanshd <ekansh.dugar@gmail.com>
2023-02-15 14:24:50 +0530

    v4.1

--- main.tf
+++ main.tf
@@ -9,11 +9,22 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-    access_key = lookup(AWS_ACCESS_KEY_ID)
-    secret_key = lookup(AWS_SECRET_ACCESS_KEY)
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
 
 resource "aws_instance" "terraform-ec2" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
+}
+
+
+locals {
+  aws_access_key = lookup(var.aws_access_key, AWS_ACCESS_KEY_ID)
+  aws_secret_key = lookup(var.aws_secret_key, AWS_SECRET_ACCESS_KEY)
 }
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "aws_access_key" {
+  description = "The AWS access key."
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key."
+}
