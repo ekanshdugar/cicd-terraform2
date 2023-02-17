ca5fd73bae86dd7e2a2b44b48688a18b10094648
ekanshd <ekansh.dugar@gmail.com>
2023-02-17 13:26:53 +0530

    v6.3

--- main.tf
+++ main.tf
@@ -20,6 +20,53 @@ resource "aws_instance" "terraform-ec2" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
   key_name      = "ansible-key1"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.example.name
+  }
   
+}
+
+
+resource "aws_iam_instance_profile" "example" {
+  name = "example-instance-profile"
+
+  role = aws_iam_role.example.name
+
+  # Allow access to the AWS metadata service
+  policy {
+    policy_document = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect    = "Allow"
+          Action    = "ec2:Describe*"
+          Resource  = "*"
+        },
+        {
+          Effect    = "Allow"
+          Action    = "ec2messages:Get*"
+          Resource  = "*"
+        },
+        {
+          Effect    = "Allow"
+          Action    = "ssm:CreateAssociation"
+          Resource  = "*"
+        },
+        {
+          Effect    = "Allow"
+          Action    = "ssm:GetParameters"
+          Resource  = "*"
+        },
+      ]
+    })
+  }
+}
+
+
+
+metadata_options {
+  http_tokens = "optional"
+  http_put_response_hop_limit = 1
+  http_endpoint = "enabled"
 }
 
