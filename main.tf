453798f54814d093f1af762e34c8dc9f4188b7bd
ekanshd <ekansh.dugar@gmail.com>
2023-02-17 14:39:10 +0530

    v6.5

--- main.tf
+++ main.tf
@@ -13,6 +13,21 @@ provider "aws" {
     secret_key = var.aws_secret_access_key
 }
 
+resource "aws_iam_role" "example" {
+  name = "example-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
 
