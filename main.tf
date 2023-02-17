f2f9742c46ffcc1b7e555d2681338648f898918a
ekanshd <ekansh.dugar@gmail.com>
2023-02-17 15:40:28 +0530

    v7.3

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-    # access_key = var.aws_access_key_id
-    # secret_key = var.aws_secret_access_key
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
 }
 
 resource "aws_iam_role" "example" {
