058244392635760733a4a3b49b1b3851cfef1205
ekanshd <ekansh.dugar@gmail.com>
2023-02-21 12:32:03 +0530

    v1.8

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "terraform-ec2" {
   instance_type        = "t2.micro"
   key_name             = "ansible-key1"
   iam_instance_profile = aws_iam_instance_profile.example.name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  security_group = aws_security_group.allow_ssh.id
 }
 
 
